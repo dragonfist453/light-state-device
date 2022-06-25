38ef9605a7a992b493ea2c4011c93e2d05a65c4c
dragonfist453 <thermosphere453@gmail.com>
2022-06-25 16:25:54 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const Gpio = require('onoff').Gpio;
+const { io } = require("socket.io-client");
+
+const hostname = 'http://localhost:8080';
+const socket = io(hostname);
+
+const lights = {
+    hallCorner: new Gpio(1, 'out'),
+    hallShortEdge: new Gpio(2, 'out'),
+    hallLongEdge: new Gpio(3, 'out'),
+    hallSmallLight: new Gpio(4, 'out'),
+    roomLight: new Gpio(5, 'out'),
+    hallRgb: new Gpio(6, 'out'),
+    roomRgb: new Gpio(7, 'out')
+};
+
+Object(lights).keys().forEach(light => {
+    console.log(light);
+})
